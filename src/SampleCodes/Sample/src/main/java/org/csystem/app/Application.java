908981246ebe2940022707087a02f2244dbbfa3a
Eyup Dogan <eyodohan@gmail.com>
2023-12-22 20:58:55 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,20 +1,23 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    var değişkenler virgül ile ayrılarak bildirilemez. Bu durumda ayrı ayrı bildirilmeleri gerekir
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-
-import com.karandev.io.util.console.Console;
-import org.csystem.util.numeric.NumberUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 class Application {
-    public static void run(String [] args)
+    public static void run(String[] args)
     {
-        long start = System.currentTimeMillis();
-        long nanoStart = System.nanoTime();
-        Console.writeLine(NumberUtil.isPrime(569_785_970_6174_583_067L) ? "Asal" : "Asal değil");
-        long end = System.currentTimeMillis();
-        long nanoEnd = System.nanoTime();
-
-        Console.writeLine("Duration:%f seconds", (end - start) / 1000.);
-        Console.writeLine("Duration:%f seconds (nanoTime)", (nanoEnd - nanoStart) / 1_000_000_000.);
+        var m = Sample.create();
     }
 }
 
+class Sample {
+    public static Map<String, ArrayList<String>> create()
+    {
+        // ...
+        return new HashMap<>();
+    }
+}
