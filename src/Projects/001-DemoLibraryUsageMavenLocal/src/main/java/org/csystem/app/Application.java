9973ed8974dbfdb0eb84f823667d6c316ea91099
Eyup Dogan <eyodohan@gmail.com>
2024-02-09 18:18:35 +0300

    update

--- src/Projects/001-DemoLibraryUsageMavenLocal/src/main/java/org/csystem/app/Application.java
+++ src/Projects/001-DemoLibraryUsageMavenLocal/src/main/java/org/csystem/app/Application.java
@@ -5,6 +5,9 @@ import com.karandev.io.util.console.Console;
 class Application {
     public static void run(String[] args)
     {
-        Console.writeLine("Hello, World");
+        int a = Console.readInt("Birinci sayıyı giriniz:", "Hatalı giriş yaptınız");
+        int b = Console.readInt("İkinci sayıyı giriniz:", "Hatalı giriş yaptınız");
+
+        Console.writeLine("%d + %d = %d", a, b, a + b);
     }
 }
