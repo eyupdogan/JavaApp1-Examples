9e2c7b29c277aed9723fdb9db66a8db1df152ec2
Eyup Dogan <eyodohan@gmail.com>
2024-02-10 18:17:39 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,24 +3,14 @@ package org.csystem.app;
 import com.karandev.io.util.console.Console;
 
 import java.math.BigInteger;
+import java.util.Random;
 
 class Application {
     public static void run(String[] args)
     {
-        while (true) {
-            var a = new BigInteger(Console.read("Birinci sayıyı giriniz:"));
-            var b = new BigInteger(Console.read("İkinci sayıyı giriniz:"));
-            var c = new BigInteger(Console.read("Üçüncü sayıyı giriniz:"));
-            var d = a.add(b);
+        var a = Console.readBigInteger("Input first number:");
 
-            if (c.equals(d))
-                break;
-
-            Console.writeLine("a = %s", a);
-            Console.writeLine("b = %s", b);
-            Console.writeLine("c = %s", c);
-            Console.writeLine("d = %s", d);
-
-        }
+        for (var i = BigInteger.ZERO; i.compareTo(BigInteger.TEN) < 0; i = i.add(BigInteger.ONE))
+            Console.writeLine("%s ", i);
     }
 }
