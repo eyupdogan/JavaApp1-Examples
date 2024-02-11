2f683c89ab52d2e8cf1aa892092254d5f5b8be02
Eyup Dogan <eyodohan@gmail.com>
2024-02-12 00:09:44 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,23 +1,29 @@
 package org.csystem.app;
 
-import org.csystem.util.console.Console;
+import com.karandev.io.util.console.Console;
 
 class Application {
     public static void run(String[] args)
     {
-        Sample.foo(100);
-    }
-}
+        while (true) {
+            var a = Console.readInt("Input a number:");
 
-class Sample {
-    public static void foo(int x)
-    {
-        Console.writeLine("Giriş %d", x);
+            Console.writeLine("a = %d", a);
+            Console.writeLine("a = 0b%s", Integer.toBinaryString(a));
+            //Console.writeLine("a = 0x%s", Integer.toHexString(a));
+            //Console.writeLine("a = 0%s", Integer.toOctalString(a));
 
-        if (x == 0)
-            return;
+            //1110_0100
+            //0001_1011
+            //0001_1100
 
-        foo(x - 1);
-        Console.writeLine("Çıkış %d", x);
+            //AEB1H 1010 1110 1011 0001
+            //2D3EH 0010 1101 0011 1110
+
+            //81AC   1000 0001 1010 1100
+
+            if (a == 0)
+                break;
+        }
     }
 }
