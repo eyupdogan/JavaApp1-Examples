c07de1895ae6097acc9d0087be5a227c3e925fbe
Eyup Dogan <eyodohan@gmail.com>
2024-02-12 20:08:49 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,16 +3,12 @@ package org.csystem.app;
 import com.karandev.io.util.console.Console;
 import org.csystem.util.bitwise.BitwiseUtil;
 
+import java.util.Scanner;
+
 class Application {
     public static void run(String[] args)
     {
-        while (true) {
-            var a = Console.readInt("Input first number:");
-            var b = Console.readInt("Input second number:");
 
-            Console.writeLine(BitwiseUtil.toBinaryStr(a));
-            Console.writeLine(BitwiseUtil.toBinaryStr(b));
-            Console.writeLine(BitwiseUtil.toBinaryStr(a ^ b));
-        }
     }
 }
+
