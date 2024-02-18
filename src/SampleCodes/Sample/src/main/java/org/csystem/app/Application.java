25604a56d32b7c76de451fe1c3e950debf36ee44
Eyup Dogan <eyodohan@gmail.com>
2024-02-19 00:23:44 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -3,13 +3,8 @@ package org.csystem.app;
 import org.csystem.util.console.Console;
 
 class Application {
-    public static void run(String...args)
+    public static void run(String [] args)
     {
-        int i;
 
-        for (i = 10; i >= 0; ++i)
-            ; //System.out.println(i);
-
-        System.out.printf("i = %d%n", i);
     }
 }
