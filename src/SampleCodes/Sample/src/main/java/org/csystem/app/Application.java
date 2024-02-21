aaaadf55e1996ae58c702beda585694ba105cac4
Eyup Dogan <eyodohan@gmail.com>
2024-02-21 15:56:52 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -1,27 +1,102 @@
 package org.csystem.app;
 
 
-import org.csystem.util.console.Console;
+import com.karandev.io.util.console.Console;
+import org.csystem.util.numeric.NumberUtil;
 
 import java.util.Random;
 
 class Application {
     public static void run(String[] args)
     {
-        java.util.Scanner kb = new java.util.Scanner(System.in);
+        var random = new Random();
 
-        while (true) {
-            System.out.print("Bir yazı giriniz:");
-            String s = kb.nextLine();
+        IIntBinaryOperator ibo = IntOperation::multiply; //(a, b) -> IntOperation.multiply(a, b);
 
-            if ("elma".equals(s))
-                break;
+        Console.writeLine(ibo.applyAsInt(3, 5));
+        Console.writeLine("------------------------------------------------------------------");
 
-            if (s.length() != 0)
-                System.out.printf("Girilen yazı:(%s)%n", s);
-            else
-                System.out.println("Boş string girdiniz!...");
-        }
+        ibo = Integer::sum;
+        Console.writeLine(ibo.applyAsInt(3, 4));
+        Console.writeLine("------------------------------------------------------------------");
+
+        IIntPredicate intPredicate = NumberUtil::isPrime;
+        Console.writeLine(intPredicate.test(1_000_003));
+        Console.writeLine("------------------------------------------------------------------");
+
+        IIntSupplier intSupplier = random::nextInt;
+        Console.writeLine(intSupplier.get());
+        Console.writeLine("------------------------------------------------------------------");
+
+        IIntRandomBoundSupplier intRandomBoundSupplier = Random::nextInt;
+        Console.writeLine(intRandomBoundSupplier.get(random, 100));
+        Console.writeLine("------------------------------------------------------------------");
+
+        IStringToIntConverter converter = String::length;
+        Console.writeLine(converter.convert("ankara"));
+        Console.writeLine("------------------------------------------------------------------");
+
+        IIntValueFactory intValueFactory = IntValue::new;
+        Console.writeLine(intValueFactory.create(4));
+        Console.writeLine("------------------------------------------------------------------");
+
+        IIntValueDefaultFactory intValueDefaultFactory = IntValue::new;
+        Console.writeLine(intValueDefaultFactory.create());
+        Console.writeLine("------------------------------------------------------------------");
     }
 }
 
+class IntOperation {
+    public static int multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+class IntValue {
+    private int m_value;
+
+    public IntValue()
+    {
+    }
+
+    public IntValue(int value)
+    {
+        m_value = value;
+    }
+
+    public int getValue()
+    {
+        return m_value;
+    }
+
+    //...
+}
+
+interface IIntValueFactory {
+    IntValue create(int val);
+}
+
+interface IIntValueDefaultFactory {
+    IntValue create();
+}
+
+interface IIntBinaryOperator {
+    int applyAsInt(int a, int b);
+}
+
+interface IIntPredicate {
+    boolean test(int a);
+}
+
+interface IIntSupplier {
+    int get();
+}
+
+interface IIntRandomBoundSupplier {
+    int get(Random random, int bound);
+}
+
+interface IStringToIntConverter {
+    int convert(String str);
+}
