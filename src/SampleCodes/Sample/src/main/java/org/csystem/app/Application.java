3917c876ebc2812a7607ddd97766be2d7eff7af7
Eyup Dogan <eyodohan@gmail.com>
2024-02-21 20:35:52 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -4,99 +4,16 @@ package org.csystem.app;
 import com.karandev.io.util.console.Console;
 import org.csystem.util.numeric.NumberUtil;
 
+import java.io.InputStream;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 class Application {
     public static void run(String[] args)
     {
+        var count = Console.readInt("Bir sayı giriniz");
         var random = new Random();
 
-        IIntBinaryOperator ibo = IntOperation::multiply; //(a, b) -> IntOperation.multiply(a, b);
-
-        Console.writeLine(ibo.applyAsInt(3, 5));
-        Console.writeLine("------------------------------------------------------------------");
-
-        ibo = Integer::sum;
-        Console.writeLine(ibo.applyAsInt(3, 4));
-        Console.writeLine("------------------------------------------------------------------");
-
-        IIntPredicate intPredicate = NumberUtil::isPrime;
-        Console.writeLine(intPredicate.test(1_000_003));
-        Console.writeLine("------------------------------------------------------------------");
-
-        IIntSupplier intSupplier = random::nextInt;
-        Console.writeLine(intSupplier.get());
-        Console.writeLine("------------------------------------------------------------------");
-
-        IIntRandomBoundSupplier intRandomBoundSupplier = Random::nextInt;
-        Console.writeLine(intRandomBoundSupplier.get(random, 100));
-        Console.writeLine("------------------------------------------------------------------");
-
-        IStringToIntConverter converter = String::length;
-        Console.writeLine(converter.convert("ankara"));
-        Console.writeLine("------------------------------------------------------------------");
-
-        IIntValueFactory intValueFactory = IntValue::new;
-        Console.writeLine(intValueFactory.create(4));
-        Console.writeLine("------------------------------------------------------------------");
-
-        IIntValueDefaultFactory intValueDefaultFactory = IntValue::new;
-        Console.writeLine(intValueDefaultFactory.create());
-        Console.writeLine("------------------------------------------------------------------");
+        IntStream.generate(()-> random.nextInt(100)).filter(NumberUtil::isPrime).limit(count).forEach(p -> Console.write("%d ", p));
     }
-}
-
-class IntOperation {
-    public static int multiply(int a, int b)
-    {
-        return a * b;
-    }
-}
-
-class IntValue {
-    private int m_value;
-
-    public IntValue()
-    {
-    }
-
-    public IntValue(int value)
-    {
-        m_value = value;
-    }
-
-    public int getValue()
-    {
-        return m_value;
-    }
-
-    //...
-}
-
-interface IIntValueFactory {
-    IntValue create(int val);
-}
-
-interface IIntValueDefaultFactory {
-    IntValue create();
-}
-
-interface IIntBinaryOperator {
-    int applyAsInt(int a, int b);
-}
-
-interface IIntPredicate {
-    boolean test(int a);
-}
-
-interface IIntSupplier {
-    int get();
-}
-
-interface IIntRandomBoundSupplier {
-    int get(Random random, int bound);
-}
-
-interface IStringToIntConverter {
-    int convert(String str);
 }
