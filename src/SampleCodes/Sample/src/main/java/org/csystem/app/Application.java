8bcdc51531fca78ef2f951d0e1818d1d43f34060
Eyup Dogan <eyodohan@gmail.com>
2024-02-27 00:03:21 +0300

    update

--- src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
+++ src/SampleCodes/Sample/src/main/java/org/csystem/app/Application.java
@@ -7,11 +7,23 @@ import com.karandev.io.util.console.CommandPrompt;
 class Application {
     public static void run(String[] args)
     {
-        CommandPrompt.createBuilder()
-                .registerObject(new CommandsInfo())
-                .setPrompt("csd")
-                .setWrongNumberOfArgumentsErrorMessage("Invalid usage")
-                .create().run();
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        while (true) {
+            System.out.print("Bir sayı giriniz:");
+            int a = Integer.parseInt(kb.nextLine());
+            long b;
+
+            b = a;
+
+            System.out.printf("a = %d%n", a);
+            System.out.printf("a = %08X%n",a);
+            System.out.printf("b = %d%n", b);
+            System.out.printf("b = %016X%n", b);
+
+            if (a == 0)
+                break;
+        }
     }
 }
 
